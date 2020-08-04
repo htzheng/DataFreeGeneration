655af028b03cc5b21a601d7968dc98da95bdb5d1
zhenght <zheng.ht.ustc@gmail.com>
2020-08-04 01:13:13 -0400

    a

--- ResNet50_IN/train_18b.sh
+++ ResNet50_IN/train_18b.sh
@@ -1,5 +1,5 @@
 python main.py -a resnet18\
-            /home/haitian/Dropbox/DataFreeGeneration/datasets/ImageNet\
+            localdisk2/yfeng23/dataset/ImageNet\
             --gpu 0\
             --arch resnet50_in\
             --batch-size 128\
